1f64e18736ed5419b35ce75dc62e1733160a1d2e
ROBMO-CLOUD <165340993+ROBMO-CLOUD@users.noreply.github.com>
2024-09-10 07:11:14 +0200

    Update and rename packages.txt to packages.bat

---
+++ Python/packages.bat
@@ -0,0 +1,25 @@
+@echo off
+echo Instalando paquetes necesarios...
+
+pip install altgraph==0.17.4
+pip install certifi==2024.8.30
+pip install charset-normalizer==3.3.2
+pip install greenlet==3.0.3
+pip install idna==3.8
+pip install packaging==24.1
+pip install pefile==2024.8.26
+pip install playwright==1.46.0
+pip install pycryptodome==3.20.0
+pip install pyee==11.1.0
+pip install pyinstaller==6.10.0
+pip install pyinstaller-hooks-contrib==2024.8
+pip install pystyle==2.9
+pip install pyTelegramBotAPI==4.22.1
+pip install pywin32-ctypes==0.2.3
+pip install setuptools==74.1.2
+pip install soupsieve==2.6
+pip install typing_extensions==4.12.2
+pip install urllib3==2.2.2
+
+echo Instalación completa.
+pause
